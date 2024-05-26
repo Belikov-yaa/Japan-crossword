4bdb906a7ee3a4bb2401fb2ac855731e28bbc661
Yaroslav <beyaroslav@gmail.com>
2024-05-26 17:27:53 +0700

    Initial commit

---
+++ src/main/java/ru/yarikbel/App.java
@@ -0,0 +1,13 @@
+package ru.yarikbel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "\u001B[35mHello \u001B[34mWorld\u001B[43m!\u001B[0m" );
+    }
+}
---
+++ src/main/java/ru/yarikbel/JapCross.java
@@ -0,0 +1,6 @@
+package ru.yarikbel;
+
+public class JapCross {
+
+
+}
