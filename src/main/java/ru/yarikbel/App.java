19bdfcd89283b16759c3286e1692161b33832219
Yaroslav <beyaroslav@gmail.com>
2024-05-29 23:37:39 +0700

    + load crossword data from a file + iterate search of 1 solution + step-to-step solution print

--- src/main/java/ru/yarikbel/App.java
+++ src/main/java/ru/yarikbel/App.java
@@ -1,13 +1,69 @@
 package ru.yarikbel;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
- * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "\u001B[35mHello \u001B[34mWorld\u001B[43m!\u001B[0m" );
+public class App {
+    public static void main(String[] args) {
+//        System.out.println("\u001B[35mHello \u001B[34mWorld\u001B[43m!\u001B[0m");
+
+        JapCross cross;
+        List<List<Integer>> columnBlocksList = new ArrayList<>();
+        List<List<Integer>> rowBlocksList = new ArrayList<>();
+
+        if (args.length > 0 && !args[0].isBlank()) {
+            List<String> allLineFromFile = readCrossDataFileToStrngList(args[0]);
+            getCrossColAndRowBlockData(allLineFromFile, columnBlocksList, rowBlocksList);
+        }
+        cross = new JapCross(columnBlocksList, rowBlocksList);
+        OptionalInt maxCol = columnBlocksList.stream().mapToInt(List::size).max();
+        OptionalInt maxRow = rowBlocksList.stream().mapToInt(List::size).max();
+//        System.out.println(maxCol.getAsInt() + " " + maxRow.getAsInt());
+//        System.out.println("###");
+//        columnBlocksList.forEach(System.out::println);
+//        System.out.println("###");
+//        rowBlocksList.forEach(System.out::println);
+        cross.run();
+//        cross.printSolution();
+    }
+
+    private static void getCrossColAndRowBlockData(List<String> lines,
+                                                   List<List<Integer>> columnBlocksList,
+                                                   List<List<Integer>> rowBlocksList) {
+        int rowAmount = Integer.parseInt(lines.get(0));
+        for (int i = 1; i <= rowAmount; i++) {
+            rowBlocksList.add(parseLineToIntegerList(lines.get(i)));
+        }
+        int colAmount = Integer.parseInt(lines.get(rowAmount + 1));
+        for (int i = rowAmount + 2; i <= colAmount + rowAmount + 1; i++) {
+            columnBlocksList.add(parseLineToIntegerList(lines.get(i)));
+        }
+    }
+
+    private static ArrayList<Integer> parseLineToIntegerList(String s) {
+        ArrayList<Integer> result = new ArrayList<>();
+        String[] numbers = s.split(" ");
+        for (String number : numbers) {
+            int block = Integer.parseInt(number);
+            if (block < 0) throw new RuntimeException("Wrong file data");
+            result.add(block);
+        }
+        return result;
+    }
+
+    public static List<String> readCrossDataFileToStrngList(String fileName) {
+        try (Scanner input = new Scanner(new File(fileName))) {
+            ArrayList<String> result = new ArrayList<>();
+            while (input.hasNext()) {
+                result.add(input.nextLine());
+            }
+            return result;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/ru/yarikbel/JapCross.java
+++ src/main/java/ru/yarikbel/JapCross.java
@@ -1,6 +1,254 @@
 package ru.yarikbel;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Math.max;
+
 public class JapCross {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+
+    // текущему состоянию клеток: 0 -- пока что неизвестно, 1 -- зарисованная, 2 -- гарантированно не зарисованная,
+    private final byte[][] gameField;
+    private final byte[][] gameFieldColor;
+    private final int rowAmount;
+    private final int columnAmount;
+    private final boolean[] needRefreshCol;
+    private final boolean[] needRefreshRow;
+    private final boolean[] canBeFilled;
+    private final boolean[] canBeEmpty;
+
+
+    private final List<List<Integer>> columnBlocksListsArray;
+    private final List<List<Integer>> rowBlocksListsArray;
+    private int errorLevel;
+
+    public JapCross(List<List<Integer>> columnBlocksListsArray, List<List<Integer>> rowBlocksListsArray) {
+        this.rowAmount = rowBlocksListsArray.size();
+        this.columnAmount = columnBlocksListsArray.size();
+        this.gameField = new byte[rowAmount][columnAmount];
+        this.gameFieldColor = new byte[rowAmount][columnAmount];
+        this.columnBlocksListsArray = columnBlocksListsArray;
+        this.rowBlocksListsArray = rowBlocksListsArray;
+        this.needRefreshCol = new boolean[columnAmount];
+        this.needRefreshRow = new boolean[rowAmount];
+        Arrays.fill(needRefreshCol, true);
+        Arrays.fill(needRefreshRow, true);
+
+        this.canBeFilled = new boolean[max(columnAmount, rowAmount)];
+        this.canBeEmpty = new boolean[max(columnAmount, rowAmount)];
+    }
+
+    public void iterateLineLook() {
+        boolean findedCellsState;
+        do {
+            findedCellsState = false;
+            for (int i = 0; i < rowAmount; i++) {
+                if (needRefreshRow[i])
+                    findedCellsState = analyzeLine(true, i) || findedCellsState;
+            }
+            System.out.println("row search");
+            if (findedCellsState) {
+                printSolution();
+                clearColorInfo();
+            }
+
+            for (int i = 0; i < columnAmount; i++) {
+                if (needRefreshCol[i]) findedCellsState = analyzeLine(false, i) || findedCellsState;
+            }
+            if (findedCellsState) {
+                System.out.println("column search");
+                printSolution();
+                clearColorInfo();
+            }
+        } while (findedCellsState);
+    }
+
+    private void clearColorInfo() {
+        for (int i = 0; i < rowAmount; i++) {
+            Arrays.fill(gameFieldColor[i], (byte) 0);
+        }
+    }
+
+    public void run() {
+        errorLevel = 0;
+        iterateLineLook();
+    }
+
+    public void printSolution() {
+        System.out.println("=============");
+        for (int i = 0; i < rowAmount; i++) {
+            byte previousColor = 0;
+            for (int j = 0; j < columnAmount; j++) {
+                if (previousColor != gameFieldColor[i][j]) {
+                    previousColor = gameFieldColor[i][j];
+                    if (gameFieldColor[i][j] == 0) {
+                        System.out.print(ANSI_RESET);
+                    } else {
+                        System.out.print(ANSI_RED);
+                    }
+                }
+                if (gameField[i][j] == 1)
+                    System.out.print("\u2588");
+                else if (gameField[i][j] == 2)
+                    System.out.print("\u2591");
+                else System.out.print("\u25ab");
+            }
+            System.out.println(ANSI_RESET);
+        }
+        System.out.println("=============");
+    }
+
+    private boolean tryBlock(int blockNumber,
+                             int blockStartIndex,
+                             byte[] cells,
+                             List<Integer> blocksLen,
+                             boolean[] canBeFilled,
+                             boolean[] canBeEmpty) {
+        int nextBlockStartIndex;
+        int blockLen = blockNumber == -1 ? 0 : blocksLen.get(blockNumber);
+        boolean result;
+        // проверяем все клеточки блока, если хоть одна из них гарантированно не зарисованная, то размещение блока в этой позиции не возможно
+        if (blockNumber > -1) {
+            for (int i = blockStartIndex; i < blockStartIndex + blockLen; i++) {
+                if (cells[i] == 2) {
+                    return false;
+                }
+            }
+        }
+        // если блок не последний в последовательности, то проверяем непротиворечивость размещения следующих за ним блоков
+        if (blockNumber + 1 < blocksLen.size()) {
+            result = false;
+            int nextBlockStartRightIndexLimit = getStartIndexLimit(blockNumber + 1, blocksLen);
+            for (nextBlockStartIndex = blockStartIndex + blockLen + 1;
+                 nextBlockStartIndex <= cells.length - nextBlockStartRightIndexLimit;
+//                 nextBlockStartIndex <= cells.length - blocksLen.get(blockNumber + 1) + 1;
+                 nextBlockStartIndex++) {
+                if (nextBlockStartIndex > 0 && cells[nextBlockStartIndex - 1] == 1) { // возможно нужно добавить вначале условие "blockNumber > -1 &&"
+                    break;
+                }
+                if (tryBlock(blockNumber + 1, nextBlockStartIndex, cells, blocksLen, canBeFilled, canBeEmpty)) {
+                    result = true;
+                    //(*какое-то непротиворечивое размещение дальнейших блоков существует*)
+                    for (int i = blockStartIndex; i < blockStartIndex + blockLen; i++) {
+                        canBeFilled[i] = true; // помечаем предполагаемые занятые блоком клетки
+                    }
+                    for (int i = blockStartIndex + blockLen; i < nextBlockStartIndex; i++) {
+                        if (i > -1)
+                            canBeEmpty[i] = true; // помечаем клетки между текущим блоком и последующим как возможно пустые
+                    }
+                }
+            }
+            return result;
+        } else { // если блок последний, то проверяем, что нет зарисованных блоков после него
+            for (int i = blockStartIndex + blockLen; i < cells.length; i++) {
+                if (cells[i] == 1) {
+                    return false;
+                }
+            }
+            // (*данное размещение последнего блока непротиворечиво*)
+            for (int i = blockStartIndex; i < blockStartIndex + blockLen; i++) {
+                canBeFilled[i] = true; // помечаем предполагаемые занятые блоком клетки
+            }
+            for (int i = blockStartIndex + blockLen; i < cells.length; i++) {
+                if (i > -1)
+                    canBeEmpty[i] = true; // помечаем клетки от текущим блоком до конца линии как возможно пустые
+            }
+            return true;
+        }
+    }
+
+    private int getStartIndexLimit(int blockNumber, List<Integer> blocksLen) {
+        int sum = 0;
+        int spaces = 0;
+        for (int i = blockNumber; i < blocksLen.size(); i++) {
+            sum += blocksLen.get(i);
+            spaces++;
+        }
+        return sum + spaces - 1;
+    }
+
+    private boolean analyzeLine(boolean isAnalyzeRow, int lineNumber) {
+        boolean result = false;
+        byte[] cells;
+        List<Integer> blocksLen;
+        if (isAnalyzeRow) {
+            needRefreshRow[lineNumber] = false;
+            cells = getCellsFromRow(lineNumber);
+            blocksLen = rowBlocksListsArray.get(lineNumber);
+        } else {
+            needRefreshCol[lineNumber] = false;
+            cells = getCellsFromColumn(lineNumber);
+            blocksLen = columnBlocksListsArray.get(lineNumber);
+        }
+
+        Arrays.fill(canBeFilled, false);
+        Arrays.fill(canBeEmpty, false);
+
+        if (tryBlock(-1, -1, cells, blocksLen, canBeFilled, canBeEmpty)) {
+            for (int i = 0; i < cells.length; i++) {
+                if (cells[i] == 0 && (canBeFilled[i] ^ canBeEmpty[i])) {
+                    result = true;
+                    if (isAnalyzeRow) {
+                        needRefreshCol[i] = true;
+                    } else {
+                        needRefreshRow[i] = true;
+                    }
+                    if (canBeFilled[i]) {
+                        cells[i] = 1;
+                    } else {
+                        cells[i] = 2;
+                    }
+                    if (isAnalyzeRow) {
+                        gameField[lineNumber][i] = cells[i];
+                        gameFieldColor[lineNumber][i] = 1;
+                    } else {
+                        gameField[i][lineNumber] = cells[i];
+                        gameFieldColor[i][lineNumber] = 1;
+                    }
+                }
+            }
+        } else {
+            errorLevel = 1;
+        }
+        return result;
+    }
+
+    private byte[] getCellsFromColumn(int lineNumber) {
+        byte[] cells = new byte[rowAmount];
+        for (int i = 0; i < rowAmount; i++) {
+            cells[i] = gameField[i][lineNumber];
+        }
+        return cells;
+    }
+
+    private byte[] getCellsFromRow(int lineNumber) {
+        byte[] cells = new byte[columnAmount];
+        System.arraycopy(gameField[lineNumber], 0, cells, 0, columnAmount);
+        return cells;
+    }
+
+    public byte[][] getGameField() {
+        return gameField;
+    }
+
+    public int getColumnAmount() {
+        return columnAmount;
+    }
+
+    public int getRowAmount() {
+        return rowAmount;
+    }
+
+    public List<List<Integer>> getColumnBlocksListsArray() {
+        return columnBlocksListsArray;
+    }
+
+    public List<List<Integer>> getRowBlocksListsArray() {
+        return rowBlocksListsArray;
+    }
 
 }
